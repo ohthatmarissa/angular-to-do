0b79a3a874a55bec355dbaf1ff990d4716ae9321
Marissa Perry <ohthatmarissa@gmail.com>
2019-06-12 10:33:36 -0700

    add task list child modulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
+  selectedTask: null;
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: null;
+  
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -25,15 +26,7 @@ export class AppComponent {
   finishedEditing() {
     this.selectedTask = null;
   }
-  
-  priorityColor(currentTask) {
-    if (currentTask.priority == 3) {
-      return 'bg-danger';
-    } else if (currentTask.priority == 2) {
-      return 'bg-warning';
-    } else {
-      return 'bg-info';
-    }
-  }
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
